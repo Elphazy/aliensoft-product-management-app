1100493eb871feceaa1d09d068f08cdb5138e0f4
Elphas Wanyonyi <52028169+Elphazy@users.noreply.github.com>
2025-11-24 16:08:11 +0300

    Add Vue Router configuration for application

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../pages/Login.vue'
+import Products from '../pages/Products.vue'
+import AddProduct from '../pages/AddProduct.vue'
+import ViewProduct from '../pages/ViewProduct.vue'
+import EditProduct from '../pages/EditProduct.vue'
+import { useAuthStore } from '../stores/auth'
+
+const routes = [
+  { path: '/login', component: Login, name: 'Login' },
+  { path: '/', redirect: '/products' },
+  { path: '/products', component: Products, name: 'Products' },
+  { path: '/products/add', component: AddProduct, name: 'AddProduct' },
+  { path: '/products/:id', component: ViewProduct, name: 'ViewProduct', props: true },
+  { path: '/products/:id/edit', component: EditProduct, name: 'EditProduct', props: true }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = useAuthStore()
+  if (!auth.token && to.path !== '/login') {
+    next('/login')
+  } else if (auth.token && to.path === '/login') {
+    next('/products')
+  } else {
+    next()
+  }
+})
+
+export default router
