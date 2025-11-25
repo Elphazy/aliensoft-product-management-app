4d79e4322e98b8d361934fa279bb28f471c10ad4
Elphazy <mulongoelphas@gmail.com>
2025-11-25 15:08:34 +0300

    Fix index.html paths for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/aliensoft-product-management-app/", // required for GitHub Pages
+  base: "/aliensoft-product-management-app/",
 });
