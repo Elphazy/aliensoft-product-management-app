9740dc43edf03f8a290c1cdb30189719bf861e6b
Elphas Wanyonyi <52028169+Elphazy@users.noreply.github.com>
2025-11-25 18:07:52 +0300

    Update App.vue
    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <router-view />
-  <div>
-    <h1 style="color: red; font-size: 24px; padding: 20px;">AlienSoft App - Testing</h1>
-    <router-view />
-  </div>
 </template>
+
 <script>
 export default {
-  name: 'App',
-  mounted() {
-    alert('App.vue mounted!')
-  }
+  name: 'App'
 }
 </script>
